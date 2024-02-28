1b9b360d9b955a76140f9a93ca90096528d201ac
Alka Dinesh Aswar <97306749+AlkaAswar@users.noreply.github.com>
2024-02-28 16:11:33 +0530

    Add files via upload

---
+++ first R Program.R
@@ -0,0 +1,14 @@
+1+1
+1*9-6
+a<-10
+a
+b<-10
+b
+a+b
+
+df<-data.frame(x=1:3,y=c('a','b','c'),z=c(34,67,54))
+df
+
+df2<-data.frame(Emp_ID=c('EP_1','EP_2','EP_3'),Name=c('A','B','C'))
+df[1,1:2]
+df[-2,1]
